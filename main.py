538d0276d0c4cb9f0776a7bc2063637239237221
Daniil Pavlov <hgh2005dan@gmail.com>
2022-02-02 19:26:50 +0300

    task number 4

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import requests
 
 coords = ['37.620070', '55.753630']
 zoom = 10
+map_types = ['map', 'sat', 'sat,skl']
 
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 
@@ -26,11 +27,13 @@ with open(map_file, "wb") as file:
 # Инициализируем pygame
 pygame.init()
 screen = pygame.display.set_mode((500, 450))
-speed = 0.005
+speed = 0.05
 screen.blit(pygame.image.load(map_file), (0, 0))
 scale = 5
 lon = coords[0]
 lat = coords[1]
+map_type = 'map'
+type_count = 0
 pygame.display.flip()
 running = True
 while running:
@@ -46,18 +49,22 @@ while running:
                     scale -= 1
             if event.key == pygame.K_UP and float(coords[1]) < 85:
                 lat = str(float(lat) + speed)
-            elif event.key == pygame.K_DOWN and float(coords[1]) > -85:
+            if event.key == pygame.K_DOWN and float(coords[1]) > -85:
                 lat = str(float(lat) - speed)
-            elif event.key == pygame.K_LEFT and float(coords[0]) < 180:
+            if event.key == pygame.K_LEFT and float(coords[0]) < 180:
                 lon = str(float(lon) - speed)
-            elif event.key == pygame.K_RIGHT and float(coords[0]) > -180:
+            if event.key == pygame.K_RIGHT and float(coords[0]) > -180:
                 lon = str(float(lon) + speed)
+            if event.key == pygame.K_TAB:
+                type_count += 1
+                map_type = map_types[type_count % 3]
+
     zoom = str(scale)
     coords = [lon, lat]
     map_api_params = {
         "ll": ",".join(coords),
         "z": zoom,
-        "l": "map"
+        "l": map_type
     }
     response = requests.get(map_api_server, params=map_api_params)
     map_file = "map.png"
